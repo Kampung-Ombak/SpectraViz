4737fad483dc2c8f7291439ddd53a6e78054c894
Kampung-Ombak <ocak.fn22@gmail.com>
2026-09-08 22:36:05 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,18 @@
-const CACHE_NAME = 'aesthetix-v1';
-const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './main.js',
-  './manifest.json',
-  'https://unpkg.com/lucide@latest'
-];
+const CACHE_NAME = 'spectraviz-v1';
 
-// Installation
+// Install event
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
-  );
   self.skipWaiting();
 });
 
-// Activation
+// Activate event
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
+  event.waitUntil(clients.claim());
 });
 
-// Fetching Assets
+// Fetch event (Network First dengan fallback ke Cache)
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
